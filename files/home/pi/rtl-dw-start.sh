326217fb8ac2d7e5ff25c9cd674f62b417ddde4e
Ross McKelvie <iam@rossmckelvie.com>
2020-07-08 19:30:12 -0500

    Optionally disable SDR. Direwolf Logs & logrotate

--- files/home/pi/rtl-dw-start.sh
+++ files/home/pi/rtl-dw-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rtl_fm -f 144.39M - | direwolf -c /home/pi/direwolf/sdr.conf -r 24000 -D 1 -
+if test -f "/boot/DISABLE_SDR"; then
+    direwolf -c /home/pi/pigate.conf -L /home/pi/aprslogs/aprs.log
+else
+    rtl_fm -f 144.39M | direwolf -c /home/pi/pigate.conf -L /home/pi/aprslogs/aprs.log
+fi
