f1acbb0519f635b554135a83d27b34bb00ff5bbd
Ross McKelvie <iam@rossmckelvie.com>
2020-07-08 22:09:59 -0500

    Rename pigate.conf to direwolf.conf to clarify configuration

--- files/home/pi/rtl-dw-start.sh
+++ files/home/pi/rtl-dw-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -f "/boot/DISABLE_SDR"; then
-    direwolf -c /home/pi/pigate.conf -L /home/pi/aprslogs/aprs.log
+    direwolf -c /home/pi/pigate-direwolf.conf -L /home/pi/aprslogs/aprs.log
 else
-    rtl_fm -f 144.39M | direwolf -c /home/pi/pigate.conf -L /home/pi/aprslogs/aprs.log
+    rtl_fm -f 144.39M | direwolf -c /home/pi/pigate-direwolf.conf -L /home/pi/aprslogs/aprs.log
 fi
